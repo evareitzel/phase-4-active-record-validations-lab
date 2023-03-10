18abd4c650f3608f1fc9c6dbf8c5301ee2116e29
Eva Reitzel <evareitzel@gmail.com>
2023-03-09 23:25:48 -0500

    commit tree d1c1c9e23e0ecb20c69a5f250a88e22681a41123

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+
+  validate :title_must_include_clickbait
+  
+  def title_must_include_clickbait
+    if title.present?
+      unless title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+        errors.add(:title, "hey no fair!")
+      end
+    end
+  end
+
 end
